0e7af0bebb1b40e085d1f82465894a6ccdf2e15f
Gustaf Sjösten <gussjos@student.chalmers.se>
2019-11-28 19:34:25 +0100

    dx ball game uploaded

---
+++ dx-ball game/CreateLevel.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+def CreateLevel(const): 
+    
+    A = np.random.rand(7,12) + const + 0.2
+    A = np.floor(A)
+    
+    filename = 'Levels/level.csv'   
+    with open(filename, "w") as f:
+        np.savetxt(f,A)
+        
+    filename = 'ArchivedLevels/level.csv'        
+    with open(filename, "w") as f:
+        np.savetxt(f,A)
+        
+#%% Read and print
+#data = np.genfromtxt(filename)
+#print(data)
+
