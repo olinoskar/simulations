1c2b8e75a6e6238b138fc346f39d76eb19cc3f43
Gustaf Sjösten <gussjos@student.chalmers.se>
2019-12-13 21:36:11 +0100

    Gustaf fixar nya träningsfiler i mappen "RunFiles" som han tycker är lättare att köra. Kör t.ex. run_all.py

--- test_dxball.py
+++ test_dxball.py
@@ -4,8 +4,8 @@ from Network import Network
 from dxball import play_game
 network = Network([77,10,10,3])
     
-network.load(path='Networks/Network_77x10x10x3_1')
-#%%
+network.load(path='stoch_and_([5,20,20,3])')
+#network.load(path='stochastic/77_inputs/20_neurons_1')
 
 score, frames_run = play_game(network,use_network=1,display_game=1,fps=50,
               max_nbr_frames=1e5, stochastic_spawning = True)
