9dbe079c1ad298d7ec8925eb21d435b720b5176a
Gustaf Sjösten <gussjos@student.chalmers.se>
2019-12-14 01:46:08 +0100

    111111111

--- test_dxball.py
+++ test_dxball.py
@@ -1,34 +1,42 @@
 #%% One way of playing the game
 
+import argparse
 from Network import Network
 from dxball import play_game
 network = Network([5,3])  # initiate
  
+def main():
 
-# Params for loading network
-stoch_bool_network = True
-inputs = 5
-nbrHiddenLayers = 1
-nbrNeuronsPerHidden = 5
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-f', "--fps", type=int, default=50, help='fps')
+	args = parser.parse_args()
 
-network.load(path='ResultsGustaf/'
-                    + 'Stoch =' + str(stoch_bool_network)
-                    + '_inputs=' + str(inputs)
-                    + '_n_hidden_layers=' + str(nbrHiddenLayers)
-                    + '_neurons=' + str(nbrNeuronsPerHidden))
-#network.load(path='ResultsGustaf/Stoch=False/inputs=5_n_hidden_layers=2_neurons=5')
+	# Params for loading network
+	stoch_bool_network = True
+	inputs = 77
+	nbrHiddenLayers = 1
+	nbrNeuronsPerHidden = 5
 
-# Params for playing the game
-fps = 200
-max_playtime = 500000
-max_nbr_frames = max_playtime*fps
-stoch_bool_game = True
-use_network = 0
+	network.load(path='ResultsGustaf/'
+						+ 'Stoch=' + str(stoch_bool_network)
+						+ '_inputs=' + str(inputs)
+						+ '_n_hidden_layers=' + str(nbrHiddenLayers)
+						+ '_neurons=' + str(nbrNeuronsPerHidden))
 
-if use_network == 0:
-	fps = 50
+	# Params for playing the game
+	fps = args.fps
+	max_playtime = 500000
+	max_nbr_frames = max_playtime*fps
+	stoch_bool_game = True
+	use_network = 1
 
-score, frames_run = play_game(network, use_network=use_network, display_game=1, fps=fps,
-              max_nbr_frames=max_nbr_frames, stochastic_spawning = stoch_bool_game)
+	if use_network == 0:
+		fps = 50
 
-print("Score="+str(score))
+	score, frames_run = play_game(network, use_network=use_network, display_game=1, fps=fps,
+				  max_nbr_frames=max_nbr_frames, stochastic_spawning = (stoch_bool_game==1))
+
+	print("Score="+str(score))
+
+if __name__ == '__main__':
+	main()
